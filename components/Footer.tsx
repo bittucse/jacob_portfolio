72c46fd41316b6d39eec343e848ea1268a9c414e
bittucse <bittucse@outlook.com>
2025-06-02 16:39:46 +0530

    changed footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,16 +2,15 @@ import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa6'
 import { socialMedia } from '@/data'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className=' w-full pt-20 pb-10' id='contact'>
-      <div className=' w-full absolute left-0 -bottom-72 min-h-96'>
-        <img src="/footer-grid.svg" alt="grid" className=' w-full h-full opacity-50 ' />
-      </div>
+    <footer className=' w-full pb-10 mb-[100px] md:mb-5' id='contact'>
+
       <div className=' flex flex-col items-center'>
             <h1 className='heading lg:max-w-[45vw]'>
-                Got any projects or <span className=' text-purple'>ideas</span> you&apos;d like to collaborate on?
+                Got any projects or <span className=' text-aqua'>ideas</span> you&apos;d like to collaborate on?
             </h1>
             <p className=' text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals</p>
             <a href="mailto:js777jacob@gmail.com">
@@ -26,9 +25,15 @@ const Footer = () => {
         <p className=' md:text-base text-sm md:font-normal font-light'>Copyright © 2025 Jacob</p>
         <div className=' flex items-center md:gap-3 gap-6'>
             {socialMedia.map((profile)=>(
-                <div key={profile.id} className=' w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 '>
+                <a
+                    key={profile.id}
+                    href={profile.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className=' w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 '
+                >
                     <img src={profile.img} alt="social-link" width={20} height={20} />
-                </div>
+                </a>
             ))}
         </div>
       </div>
@@ -37,4 +42,3 @@ const Footer = () => {
 }
 
 export default Footer
- 
